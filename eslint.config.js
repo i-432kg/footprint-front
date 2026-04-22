5bc0295508e1e4a9257329a85437d16dfd91305c
432kg <432kg.biz@gmail.com>
2026-04-22 21:42:03 +0900

    OxLintを削除しESLintに統一

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import pluginOxlint from 'eslint-plugin-oxlint'
 
 export default defineConfig([
   {
@@ -22,6 +21,4 @@ export default defineConfig([
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-
-  ...pluginOxlint.configs['flat/recommended'],
 ])
